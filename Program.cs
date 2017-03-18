400b9f1c3306cd28e7c58ec523890c68e46068a7
Mohsen Karimi <Mohsen Karimi>
2017-03-18 09:12:47 +0330

    core

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sample_threading_async_await
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # sample-threading-async-await
+
+
+<code>dotnet restore</code><br>
+<code>dotnet build</code><br>
+<code>dotnet run</code><br>
